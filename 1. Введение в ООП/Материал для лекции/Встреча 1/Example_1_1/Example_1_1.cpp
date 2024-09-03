70f89f55ea1572514c9164d9dfca54faf906957a
Андрей Шестаков <ShestakovAD2001@yandex.ru>
2024-09-03 23:52:44 +0300

    Материал для 1 пары

---
+++ 1. Введение в ООП/Материал для лекции/Встреча 1/Example_1_1/Example_1_1.cpp
@@ -0,0 +1,86 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <conio.h>
+
+using namespace std;
+
+// максимальная длина имени
+const int maxNameLength = 20;
+
+// число оценок
+const int markCount = 3;
+
+// определение структуры данных 
+// ------------------------------------
+// Студент
+struct Student
+{
+    // ФИО 
+    char name[maxNameLength];
+
+    // оценки 
+    int marks[markCount];
+};
+// ------------------------------------
+// конец определения структуры данных
+
+// функции обработки структуры данных
+// ------------------------------------
+// заполнение данных о студенте
+void initStudent(Student& student,
+    const char* name,
+    const int marks[])
+{
+    strcpy_s(student.name, maxNameLength, name);
+    for (int i = 0; i < markCount; i++)
+    {
+        student.marks[i] = marks[i];
+    }
+}
+
+// вычисление средней оценки
+double averMark(Student student)
+{
+    double sum = 0;
+    for (int i = 0; i < markCount; i++)
+    {
+        sum += student.marks[i];
+    }
+    return sum / markCount;
+}
+
+// вывод данных о студенте
+void printStudent(Student student)
+{
+    cout << student.name << endl;
+    cout << "Оценки: ";
+    for (int i = 0; i < markCount; i++)
+    {
+        cout << setw(4) << student.marks[i];
+    }
+    cout << endl;
+}
+// ------------------------------------
+// конец списка функций обработки структуры данных
+
+int main()
+{
+    setlocale(LC_ALL, "");
+
+    cout << "Успеваемость студента." << endl << endl;
+
+    Student student;
+
+    const char* studentName{ "Петров А.О." };
+    int studentMarks[]{ 4,4,3 };
+
+    initStudent(student, studentName, studentMarks);
+
+    printStudent(student);
+
+    cout << "Средняя оценка: " << fixed << setprecision(2) << averMark(student) << endl;
+
+    _getch();
+
+    return 0;
+}
---
+++ 1. Введение в ООП/Материал для лекции/Встреча 1/Example_1_3/Example_1_3.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <vector>
+#include <conio.h>
+
+using namespace std;
+
+// определение класса 
+class Student
+{
+public:
+    string name;
+    vector<size_t> marks;
+};
+
+int main()
+{
+    setlocale(LC_ALL, "");
+
+    cout << "Успеваемость студента." << endl << endl;
+
+    // определение объекта
+    Student student;
+
+    // присвоение значений объекту
+    student.name = "Петров А.И";
+    student.marks = vector<size_t>(10, 20);
+
+    // получение значений объекта
+    cout << student.name << " : " << student.marks[0] << endl;
+
+    _getch();
+    return 0;
+}
