552ae328ce32b614e65e1e3e614023286ec79d32
Андрей Шестаков <ShestakovAD2001@yandex.ru>
2024-10-04 17:27:58 +0300

    Дополнительный пример

---
+++ 5. Шаблоны классов, класс string/Материал для лекции/Встреча 1/Example_5_1_3/Example_5_1_3.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+
+template <class T>
+void getValue(std::string prompt, T& value)
+{
+	cout << prompt;
+	cin >> value;
+	while (cin.fail())
+	{
+		cin.clear();
+		cin.ignore(32767, '\n');
+		cout << "Error... try again" << endl;
+		cout << prompt;
+		cin >> value;
+	}
+	string endLine;
+	getline(cin, endLine);
+}
+
+int main()
+{
+	int number;
+	getValue("Enter number: ", number);
+
+	std::string name;
+	getValue("Enter name: ", name);
+}
